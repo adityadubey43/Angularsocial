72addd46939c3fb5522ae17ae76286d16647fd44
Aditya dubey <aditya.p.a43@gmail.com>
2022-07-27 18:37:25 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { ServicesComponent } from './components/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LandingpageComponent},
+  {path:'services',component:ServicesComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { ServicesComponent } from './components/services/services.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    ServicesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
